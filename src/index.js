35d6239ca62682f142849d1f1479aeaf0b0c16ff
janoc0504 <janoc0504@gmail.com>
2024-05-10 13:19:31 -0600

    next commit hw-42

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
+// index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <React.StrictMode>
         <Provider store={store}>
             <App />
         </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
+
